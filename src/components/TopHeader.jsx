bc523956de90c5becec064750f4245edbbd0b623
Tomo <tomobratic23@gmail.com>
2024-01-05 19:12:42 +0100

    first commit file structure and tailwind

---
+++ src/components/TopHeader.jsx
@@ -0,0 +1,5 @@
+function TopHeader() {
+  return <div></div>;
+}
+
+export default TopHeader;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import TopHeader from "../components/TopHeader";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <TopHeader />
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default Home;
