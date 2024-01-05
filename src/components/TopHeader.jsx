b0576a825f96fa0d88e3e494d848a2689186a037
Tomo <tomobratic23@gmail.com>
2024-01-05 19:20:10 +0100

    top container added

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,5 +1,17 @@
-function TopHeader() {
-  return <div></div>;
+function TopFooter() {
+  return (
+    <div className="flex justify-around bg-black">
+      <div className=" mx-auto flex justify-between gap-5 text-white">
+        <p>
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+        </p>
+        <a href="#">Shop Now</a>
+      </div>
+      <div>
+        <p className="text-white">English</p>
+      </div>
+    </div>
+  );
 }
 
-export default TopHeader;
+export default TopFooter;
