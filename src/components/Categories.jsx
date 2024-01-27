5c108b989383649874184072667ca694a5f43119
Tomo <tomobratic23@gmail.com>
2024-01-27 20:06:48 +0100

    line added to categories section

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,10 +4,11 @@ import cellphone from "../assets/cellphone.png";
 import watch from "../assets/watch.png";
 import headphones from "../assets/headphones.png";
 import gamepad from "../assets/gamepad.png";
+import Line from "./Line";
 
 function Categories() {
   return (
-    <section className="relative my-10">
+    <section className="relative my-12 pb-16">
       <RedBorder>Categories</RedBorder>
       <h2 className="text-3xl font-bold ">Browse By Categories</h2>
       <div className="flex gap-5 justify-between mt-10">
@@ -32,6 +33,7 @@ function Categories() {
           <p>GamePad</p>
         </div>
       </div>
+      <Line />
     </section>
   );
 }
--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,5 +1,5 @@
 function Line() {
-  return <div className=" h-px w-full  absolute bottom-0 bg-[#DADADA] "></div>;
+  return <div className="h-px w-full absolute bottom-0 bg-[#DADADA]"></div>;
 }
 
 export default Line;
