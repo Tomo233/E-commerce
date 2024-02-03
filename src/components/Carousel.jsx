c2f454343fd3e740310603ce79a56a7f0d9165f9
Tomo <tomobratic23@gmail.com>
2024-02-03 14:46:31 +0100

    addToCart created need transition on hover

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react/prop-types */
 import { Carousel } from "primereact/carousel";
 import Rating from "./Rating";
@@ -5,10 +6,10 @@ import Rating from "./Rating";
 export default function ResponsiveDemo({ products }) {
   const productTemplate = (product) => {
     const itemRating = product?.rating?.rate?.toFixed();
-
+    const newPrice = product.price - product.price * 0.2;
     return (
-      <div className="flex items-center justify-center gap-4 mt-5 max-w-md cursor-pointer">
-        <div className="mb-3">
+      <div className="flex items-center flex-col justify-center gap-4 mt-10 max-w-md cursor-pointer relative ">
+        <div className="">
           <img
             src={product?.image}
             className="w-36 h-36"
@@ -17,9 +18,18 @@ export default function ResponsiveDemo({ products }) {
         </div>
         <div className="w-56">
           <h4 className="font-medium">{product?.title}</h4>
-          <h6 className="text-red-700 font-medium">${product?.price}</h6>
+          <div className="flex gap-5 my-2">
+            <h6 className="text-red-700 font-medium">${newPrice.toFixed(2)}</h6>
+            <h6 className="text-stone-400 line-through font-medium">
+              ${product?.price.toFixed(2)}
+            </h6>
+          </div>
           <div className="mt-5 flex flex-wrap gap-2 justify-content-center"></div>
           <Rating value={itemRating} />
+        </div>
+
+        <div className="bg-violet-800 w-80 h-10 absolute bottom-0 flex items-center justify-center">
+          <p className="">Add To Cart</p>
         </div>
       </div>
     );
