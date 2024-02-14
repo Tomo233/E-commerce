b0364af714dc010aa79d4a188863b00d3fcfb3d9
Tomo <tomobratic23@gmail.com>
2024-02-14 14:10:34 +0100

    first view of section created title, description ,rating and price are completly styled

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,6 @@
 import { Rating as ReactRating } from "primereact/rating";
 
-export default function Rating(value) {
+export default function Rating({ value }) {
   return (
     <div className="card flex justify-content-center mt-3">
       <ReactRating value={value} readOnly cancel={false} />
--- src/features/products/Product.jsx
+++ src/features/products/Product.jsx
@@ -1,3 +1,8 @@
+import Rating from "../../components/Rating";
+import blackDelivery from "../../assets/blackDelivery.png";
+import returnIcon from "../../assets/return.png";
+import Line from "../../components/Line";
+
 const product = {
   id: 1,
   title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
@@ -9,7 +14,49 @@ const product = {
 };
 
 function Product() {
-  return <section></section>;
+  return (
+    <section className="flex justify-between mt-14">
+      <div>
+        <img src={product.image} className="w-64" alt="" />
+      </div>
+      <div className="grid">
+        <div className="border-b border-stone-400 mb-5">
+          <h2 className="text-3xl font-semibold">{product.title}</h2>
+          <Rating value="3" />
+          <p className="text-2xl my-2">${product.price}</p>
+          <p className="w-96 mb-5 font-medium">{product.description}</p>
+        </div>
+
+        <div className="flex gap-7">
+          <input
+            type="number"
+            className="bg-stone-100 border border-stone-500  w-18 h-10 text-center"
+            min={1}
+          />
+          <button className="bg-red-500 px-5 py-3">Add To Cart</button>
+        </div>
+        <div className="flex border border-stone-500 py-5">
+          <img src={blackDelivery} className="w-12" alt="delivery" />
+          <div>
+            <h4 className="text-stone-900 font-semibold">Free Delivery</h4>
+            <p className="underline text-stone-900 font-semibold">
+              Enter your postal code for Delivery Availability
+            </p>
+          </div>
+        </div>
+
+        <div className="flex border border-stone-500 py-5">
+          <img src={returnIcon} className="w-12" alt="returnIcon" />
+          <div>
+            <h3 className="text-stone-900 font-semibold">Return Delivery</h3>
+            <p className="underline text-stone-900 font-semibold">
+              Free 30 Days Delivery Returns. Details
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Product;
