d0e5b5afaf261e1ec14bf82f88a969f449c21448
Tomo <tomobratic23@gmail.com>
2024-02-26 14:25:55 +0100

    some paddings and margins are added to whole page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,19 +6,19 @@ import fourthService from "../assets/fourthService.png";
 
 function About() {
   return (
-    <section className="pb-40">
+    <section className="pb-20">
       <div className="flex justify-between items-center mt-16">
         <div>
           <h2 className="text-5xl font-semibold">Our Story</h2>
-          <div className="max-w-lg mt-5">
-            <p className="font-medium tracking-wide">
+          <div className="max-w-lg mt-8">
+            <p className="font-medium tracking-wider">
               Launced in 2015, Exclusive is South Asia’s premier online shopping
               makterplace with an active presense in Bangladesh. Supported by
               wide range of tailored marketing, data and service solutions,
               Exclusive has 10,500 sallers and 300 brands and serves 3 millioons
               customers across the region.
             </p>
-            <p className="font-medium tracking-wide pt-3">
+            <p className="font-medium tracking-wider pt-3">
               Exclusive has more than 1 Million products to offer, growing at a
               very fast. Exclusive offers a diverse assotment in categories
               ranging from consumer.
@@ -27,7 +27,7 @@ function About() {
         </div>
         <img src={about} alt="about" />
       </div>
-      <div className="flex justify-between items-center mt-20">
+      <div className="flex justify-between items-center mt-24">
         <div className="border border-stone-300 w-64 h-52 py-3">
           <div className="grid place-items-center mt-5">
             <img src={firstService} alt="" />
