ac514e6b9073c3df485b215e7bd06cfa0ea05ccc
Tomo <tomobratic23@gmail.com>
2024-02-28 14:18:07 +0100

    Error Page redesigned

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,15 @@
-import { useRouteError } from "react-router-dom";
-
 function Error() {
-  const error = useRouteError();
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
-    </div>
+    <section className="pb-56">
+      <div className="mt-40">
+        <h1 className="font-semibold text-7xl mb-5 text-center">
+          404 Not Found
+        </h1>
+        <p className="font-medium tracking-wider text-center">
+          Your visited page not found. You may go home page.
+        </p>
+      </div>
+    </section>
   );
 }
 
