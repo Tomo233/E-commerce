33d470cf83dc5fc110d0e59ef55243aa2b9a3a42
Tomo <tomobratic23@gmail.com>
2024-03-02 16:21:57 +0100

    flex and grid are added to signUp page and some additional style for buttons and inputs

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,34 +4,38 @@ import google from "../assets/googleLogin.png";
 
 function SignUp() {
   return (
-    <section>
+    <section className="flex justify-between items-center">
       <img src={image} alt="" />
-      <div>
+      <div className="grid gap-y-3 w-1/3">
         <h2 className="text-3xl font-semibold">Create an account</h2>
         <p className="font-medium tracking-wide">Enter your details below</p>
         <input
           type="text"
           placeholder="Name"
-          className="bg-transparent w-56 h-10 border-b border-stone-900 text-left outline-none"
+          className="bg-transparent h-12 border-b border-stone-900 text-left outline-none"
         />
         <input
           type="email"
           placeholder="Email"
-          className="bg-transparent w-56 h-10 border-b border-stone-900 text-left outline-none"
+          className="bg-transparent h-12 border-b border-stone-900 text-left outline-none"
         />
         <input
           type="password"
           placeholder="Password"
-          className="bg-transparent w-56 h-10 border-b border-stone-900 text-left outline-none"
+          className="bg-transparent h-12 border-b border-stone-900 text-left outline-none"
         />
-        <button className="bg-red-500 py-4 px-20">Create Account</button>
-        <button className="border border-stone-900 py-3 px-10">
-          <img src={google} alt="" />
+        <button className="bg-red-500 py-4 mt-5 mb-2 rounded-sm text-stone-100 font-medium">
+          Create Account
         </button>
-        <p className="text-stone-600 tracking-wide">Already have account?</p>
-        <Link className="text-stone-700 underline pb-5 tracking-wide">
-          Log in
-        </Link>
+        <button className="border border-stone-400 rounded-sm py-4">
+          <img src={google} className="mx-auto" alt="" />
+        </button>
+        <div className="flex justify-center items-center gap-5 mt-2">
+          <p className="text-stone-600 tracking-wide">Already have account?</p>
+          <Link className="text-stone-700 underline  tracking-wide">
+            Log in
+          </Link>
+        </div>
       </div>
     </section>
   );
