a8ef2ad82bf9fbf53a4393577a58460fe7230702
Tomo <tomobratic23@gmail.com>
2024-03-02 16:02:22 +0100

    first html added to page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
 import image from "../assets/loginImage.png";
 function SignUp() {
   return (
     <section>
       <img src={image} alt="" />
+      <div>
+        <h2>Create an account</h2>
+        <p>Enter your details below</p>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Create Account</button>
+        <button>Sign Up With Google</button>
+        <p>Already have account</p>
+        <Link>Log in</Link>
+      </div>
     </section>
   );
 }
